c9392d97c20b650cc2a62c4397eeee9f58ae18bc
bufudanqing <feixue0213@live.com>
2018-03-13 17:19:15 +0800

    router

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,26 @@
 import React, { Component}  from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
 import { Router, Route, hashHistory } from 'react-router'
 
-render((
-    <Router history = {hashHistory}>
-        <Route path="/" component={App} />
+const Loading = () => <div>Loading...</div>
+
+const Home = Loadable({
+    loader: () => import('./routes/Home'),
+    loading:Loading,
+});
+
+const About = Loadable({
+    loader: () => import('./routes/About'),
+    loading:Loading, 
+});
+
+const App = () => (
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About} />
+        </Switch>
     </Router>
-), document.getElementById('app'));
+)
+
